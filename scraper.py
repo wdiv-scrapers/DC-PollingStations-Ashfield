c50298076b9f95414d1bf1688763ee55fd82c344
chris48s <chris.shaw480@gmail.com>
2017-10-01 11:18:39 +0100

    pass an extension so we can sync raw file to GitHub

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ districts_url = "http://www.ashfield.gov.uk/media/1966/polling-districts.zip"
 council_id = 'E07000170'
 
 
-stations_scraper = HashOnlyScraper(stations_url, council_id, 'stations')
+stations_scraper = HashOnlyScraper(stations_url, council_id, 'stations', 'xlsx')
 stations_scraper.scrape()
-districts_scraper = HashOnlyScraper(districts_url, council_id, 'districts')
+districts_scraper = HashOnlyScraper(districts_url, council_id, 'districts', 'zip')
 districts_scraper.scrape()
